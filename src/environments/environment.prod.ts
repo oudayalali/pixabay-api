ea6802681d00dc8b1a9b9ad3f699e73574f4e028
Ouday <ouday.alali@gmail.com>
2022-03-07 15:00:20 +0200

    Angular app retrieves data from pixabay api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  PIXABAY_API_KEY:'14893745-728eb841ccd5ae28e2b24958c',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   PIXABAY_API_KEY:'14893745-728eb841ccd5ae28e2b24958c',
   PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };
